802e32a6fb0fff23ab70f0d2cbd5b33b16e6294f
Fenrir7Asteron <b.fedotov@innopolis.ru>
2019-02-21 15:45:34 +0000

    Homework #5, start

---
+++ week05/common.h
@@ -0,0 +1,13 @@
+typedef struct _test_struct{
+    char name[32];
+    unsigned int age;
+    char group[8];
+} test_struct_t;
+
+
+typedef struct result_struct_{
+    char name[32];
+    unsigned int age;
+    char group[8];
+
+} result_struct_t;
