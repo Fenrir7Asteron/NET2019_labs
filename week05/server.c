6482fb4980bb8370c54680ced04562e94a538f02
Fenrir7Asteron <b.fedotov@innopolis.ru>
2019-02-21 19:36:32 +0000

    1234

--- week05/server.c
+++ week05/server.c
@@ -1,5 +1,6 @@
 //Taken from Abhishek Sagar
 
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,11 +15,40 @@
 #include <arpa/inet.h>
 
 /*Server process is running on this port no. Client has to send data to this port no*/
-#define SERVER_PORT     2000
+#define SERVER_PORT 2000
 
+char data_buffer[1024];
 test_struct_t test_struct;
 result_struct_t res_struct;
-char data_buffer[1024];
+
+struct address {
+	int sockfd;
+	test_struct_t *client_data;
+	struct sockaddr_in client_addr;
+};
+
+void* serve_client(void* args) {
+	printf("Hello from new thread!\nI will sleep for 10 seconds.\n");
+	sleep(5);
+
+	struct address* address = (struct address*) args;
+
+	int sockfd = address->sockfd;
+	test_struct_t *client_data = address->client_data;
+	struct sockaddr_in client_addr = address->client_addr;
+
+	result_struct_t result;
+	strcpy(result.name, client_data->name);
+        result.age = 12;
+	strcpy(result.group, client_data->group);
+
+                /* Server replying back to client now*/
+        int sent_recv_bytes = sendto(sockfd, (char *) &result, sizeof(result_struct_t), 0,
+                                         (struct sockaddr *) &client_addr, sizeof(struct sockaddr));
+
+        printf("Server sent %d bytes in reply to client\n", sent_recv_bytes);
+                /*Goto state machine State 3*/
+}
 
 void
 setup_udp_server_communication() {
@@ -33,7 +63,7 @@ setup_udp_server_communication() {
 
     /*client specific communication socket file descriptor,
      * used for only data exchange/communication between client and server*/
-    int comm_socket_fd = 0;
+    //int comm_socket_fd = 0;
     /* Set of file descriptor on which select() polls. Select() unblocks whever data arrives on
      * any fd present in this set*/
     fd_set readfds;
@@ -77,16 +107,7 @@ setup_udp_server_communication() {
     else
         printf("port number %d\n", ntohs(sin.sin_port));
 
-
-    /*Step 4 : Tell the Linux OS to maintain the queue of max length to Queue incoming
-     * client connections.*/
-    //if (listen(master_sock_udp_fd, 5) < 0) {
-    //    printf("listen failed\n");
-    //    return;
-    //}
-
     /* Server infinite loop for servicing the client*/
-
     while (1) {
 
         /*Step 5 : initialze and dill readfds*/
@@ -94,7 +115,6 @@ setup_udp_server_communication() {
         FD_SET(master_sock_udp_fd, &readfds);  /*Add the socket to this set on which our server is running*/
 
         printf("blocked on select System call...\n");
-
 
         /*Step 6 : Wait for client connection*/
         /*state Machine state 1 */
@@ -104,25 +124,8 @@ setup_udp_server_communication() {
 
         /*Some data on some fd present in set has arrived, Now check on which File descriptor the data arrives, and process accordingly*/
         if (FD_ISSET(master_sock_udp_fd, &readfds)) {
-            /*Data arrives on Master socket only when new client connects with the server (that is, 'connect' call is invoked on client side)*/
-            printf("New connection recieved recvd, accept the connection. Client and Server completes TCP-3 way handshake at this point\n");
+            printf("New connection recieved.\n");
 
-            /* step 7 : accept() returns a new temporary file desriptor(fd). Server uses this 'comm_socket_fd' fd for the rest of the
-             * life of connection with this client to send and recieve msg. Master socket is used only for accepting
-             * new client's connection and not for data exchange with the client*/
-            /* state Machine state 2*/
-            comm_socket_fd = accept(master_sock_udp_fd, (struct sockaddr *) &client_addr, &addr_len);
-            if (comm_socket_fd < 0) {
-
-                /* if accept failed to return a socket descriptor, display error and exit */
-                printf("accept error : errno = %d\n", errno);
-                exit(0);
-            }
-
-            printf("Connection accepted from client : %s:%u\n",
-                   inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
-            while (1) {
                 printf("Server ready to service client msgs.\n");
                 /*Drain to store client info (ip and port) when data arrives from client, sometimes, server would want to find the identity of the client sending msgs*/
                 memset(data_buffer, 0, sizeof(data_buffer));
@@ -133,47 +136,20 @@ setup_udp_server_communication() {
                 /*Like in client case, this is also a blocking system call, meaning, server process halts here untill
                  * data arrives on this comm_socket_fd from client whose connection request has been accepted via accept()*/
                 /* state Machine state 3*/
-                sent_recv_bytes = recvfrom(comm_socket_fd, (char *) data_buffer, sizeof(data_buffer), 0,
+                sent_recv_bytes = recvfrom(master_sock_udp_fd, (char *) data_buffer, sizeof(data_buffer), 0,
                                            (struct sockaddr *) &client_addr, &addr_len);
 
                 /* state Machine state 4*/
                 printf("Server recvd %d bytes from client %s:%u\n", sent_recv_bytes,
                        inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		
+		struct address* args = malloc(sizeof(struct address));
+		args->sockfd = master_sock_udp_fd;
+		args->client_data = (test_struct_t *) data_buffer;
+		args->client_addr = client_addr;
 
-                if (sent_recv_bytes == 0) {
-                    /*If server recvs empty msg from client, server may close the connection and wait
-                     * for fresh new connection from client - same or different*/
-                    close(comm_socket_fd);
-                    break; /*goto step 5*/
-
-                }
-
-                test_struct_t *client_data = (test_struct_t *) data_buffer;
-
-                /* If the client sends a special msg to server, then server close the client connection
-                 * for forever*/
-                /*Step 9 */
-                if (client_data->age == 0) {
-
-                    close(comm_socket_fd);
-                    printf("Server closes connection with client : %s:%u\n", inet_ntoa(client_addr.sin_addr),
-                           ntohs(client_addr.sin_port));
-                    /*Goto state machine State 1*/
-                    break;/*Get out of inner while loop, server is done with this client, time to check for new connection request by executing selct()*/
-                }
-
-                result_struct_t result;
-		strcpy(result.name, client_data->name);
-                result.age = 12;
-		strcpy(result.group, client_data->group);
-
-                /* Server replying back to client now*/
-                sent_recv_bytes = sendto(comm_socket_fd, (char *) &result, sizeof(result_struct_t), 0,
-                                         (struct sockaddr *) &client_addr, sizeof(struct sockaddr));
-
-                printf("Server sent %d bytes in reply to client\n", sent_recv_bytes);
-                /*Goto state machine State 3*/
-            }
+		pthread_t thread;
+		pthread_create(&thread, NULL, serve_client, (void *) args);
         }
     }/*step 10 : wait for new client request again*/
 }
